d12d6f5e3ad923229fdd857788e827322756388a
VisaNotFoundException <enzogabrielguido6@gmail.com>
2026-02-12 14:43:06 -0300

    commit

--- src/main/java/ejemplo/controller/TarjetaUsuarioController.java
+++ src/main/java/ejemplo/controller/TarjetaUsuarioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//prueba rama develop
 @RestController
 @RequestMapping("/api/tarjetas")
 public class TarjetaUsuarioController {
